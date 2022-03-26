6ee7eca4a83e2c731dda91083d5f10cf87cb0f30
0xMako <mako@eigenvector.net>
2022-03-27 00:16:01 +0100

    Utilize Pedersen Hash from circomlib (likely the old pedersen hash

--- web/src/utils/covalent.js
+++ web/src/utils/covalent.js
@@ -7,7 +7,7 @@ export async function covalentQuery() {
     let results
     try {
         // Note: gets 10,000 objects at once (arbitrary limit for hackathon)
-        const resp = await axios.get("https://api.covalenthq.com/v1/43114/tokens/0x8729438eb15e2c8b576fcc6aecda6a148776c0f5/token_holders/?&key=ckey_b5951d05b0ac41b1a37ed81af21&page-size=1000")
+        const resp = await axios.get("https://api.covalenthq.com/v1/43114/tokens/0x8729438eb15e2c8b576fcc6aecda6a148776c0f5/token_holders/?&key=ckey_b5951d05b0ac41b1a37ed81af21&page-size=10")
         let { items } = resp.data.data
         results = items.map(x => x.address)
     } catch (e) {
